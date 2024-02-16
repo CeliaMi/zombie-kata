2e44205c8158952259f92c104f0eb0add114548c
Celia <celiamillanalemany@gmail.com>
2024-02-16 12:17:55 +0100

    failing second test

--- src/test/survivalzombie.test.js
+++ src/test/survivalzombie.test.js
@@ -4,4 +4,9 @@ describe('Survival', () => {
     test('Every survivor should have a Name', ()=> {
         expect(new Survival('pepe')).toMatchObject( {"name": "pepe"});
     })
+
+    test('Each survivor should start with 0 wounds.', ()=> {
+        expect(new Survival('maría')).toMatchObject( {"name": "maría", "injured": 0});
+    })
 })
+
