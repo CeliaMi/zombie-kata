dda908e6931fcd89db4ef080c1d27bf789231269
Celia <celiamillanalemany@gmail.com>
2024-02-16 12:29:35 +0100

    creating third test

--- src/test/survivalzombie.test.js
+++ src/test/survivalzombie.test.js
@@ -8,5 +8,11 @@ describe('Survival', () => {
     test('Each survivor should start with 0 wounds.', ()=> {
         expect(new Survival('maría')).toMatchObject( {"name": "maría", "injured": 0});
     })
+
+    test('A Survivor who receives 2 Wounds should die', ()=> {
+        expect(new Survival('juan')).toMatchObject( {"name": "juan", "injured": 3, "alive": false});
+    })
+
+
 })
 
