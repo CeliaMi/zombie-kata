1334f5522d4d850ffc3392fed09b68a4d71a107a
Celia <celiamillanalemany@gmail.com>
2024-02-16 12:43:48 +0100

    fourth test failed

--- src/survivalzombie.js
+++ src/survivalzombie.js
@@ -1,8 +1,14 @@
 class Survival{
-    constructor(name, injured = 0 ){
+    constructor(name, injured = 0, alive = true){
         this.name = name;
         this.injured = injured
-    }
-}
+        if(injured === 2){
+        this.alive = false;
+        } else{
+        this.alive = alive;
+        }
+    
+
+}}
 
 module.exports = Survival;
--- src/test/survivalzombie.test.js
+++ src/test/survivalzombie.test.js
@@ -5,12 +5,16 @@ describe('Survival', () => {
         expect(new Survival('pepe')).toMatchObject( {"name": "pepe"});
     })
 
-    test('Each survivor should start with 0 wounds.', ()=> {
+    test('Each survivor should start with 0 wounds', ()=> {
         expect(new Survival('maría')).toMatchObject( {"name": "maría", "injured": 0});
     })
 
     test('A Survivor who receives 2 Wounds should die', ()=> {
-        expect(new Survival('juan')).toMatchObject( {"name": "juan", "injured": 3, "alive": false});
+        expect(new Survival('juan', 2)).toMatchObject( {"name": "juan", "injured": 2, "alive": false});
+    })
+
+    test('Each Survivor should start with the ability to perform 3 actions per shift', ()=> {
+        expect(new Survival('juan',)).toMatchObject( {"name": "juan",  "injured": 0, "alive": true, "actions": 3});
     })
 
 
